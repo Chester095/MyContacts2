33124164c3d894b58d1fa46633a3093f90e9e368
Chester095 <50018523+Chester095@users.noreply.github.com>
2021-01-18 14:49:39 +0300

    Создан ContactAdapter.java

--- app/src/main/java/com/example/mycontacts/ContactsAdapter.java
+++ app/src/main/java/com/example/mycontacts/ContactsAdapter.java
@@ -1,7 +1,10 @@
 package com.example.mycontacts;
 
 import android.content.Context;
-import android.provider.ContactsContract;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -15,5 +18,57 @@ public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.MyView
     private ArrayList<Contact> contacts;
     private MainActivity mainActivity;
 
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView nameTextView;
+        public TextView familyTextView;
+        public TextView emailTextView;
+        public TextView phoneTextView;
+
+
+        public MyViewHolder(View view) {
+            super(view);
+            nameTextView = view.findViewById(R.id.nameTextView);
+            familyTextView = view.findViewById(R.id.familyTextView);
+            emailTextView = view.findViewById(R.id.emailTextView);
+            phoneTextView = view.findViewById(R.id.phoneTextView);
+
+        }
+    }
+
+    public ContactsAdapter(Context context, ArrayList<Contact> contacts, MainActivity mainActivity) {
+        this.context = context;
+        this.contacts = contacts;
+        this.mainActivity = mainActivity;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.contacts_list_item, parent, false);
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, final int position) {
+        final Contact contact = contacts.get(position);
+        holder.nameTextView.setText(contact.getName());
+        holder.familyTextView.setText(contact.getFamily());
+        holder.emailTextView.setText(contact.getEmail());
+        holder.phoneTextView.setText(contact.getPhone());
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                mainActivity.addAndEditContacts(true, contact, position);
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return contacts.size();
+    }
 
 }
